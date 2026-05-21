ccdce57a17d935cc01a2b5d2f0c630952ddf92e2
Harmeet Sing <harmeetxx7@gmail.com>
2026-05-21 15:39:16 +0530

    lol

--- theurbancloset/settings.py
+++ theurbancloset/settings.py
@@ -112,7 +112,7 @@ EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
 EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
 DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL', default='TheUrbanCloset <noreply@theurbancloset.com>')
-
+EMAIL_TIMEOUT = int(os.getenv("EMAIL_TIMEOUT", 10))
 
 # Store Settings
 STORE_NAME = config('STORE_NAME', default='TheUrbanCloset')
